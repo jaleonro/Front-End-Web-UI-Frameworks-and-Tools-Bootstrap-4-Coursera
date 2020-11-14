5ebdebf3b7e2ddf73253866d9c75e09cf5c4596f
Johnathan Andrés León Rodriguez <Johnathan.Leon@ibm.com>
2020-11-13 20:23:51 -0500

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#reserveBtn").click(function(){
+        $('#reserveModal').modal('show');
+    });
+    $("#loginLink").click(function(){
+        $('#loginModal').modal('show');
+    });
+    $("#mycarousel").carousel( { interval: 3000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
